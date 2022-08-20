6b203cc12bb51946426fdf5269f2b5aa8065eda0
SamarV-121 <samarvispute121@pm.me>
2022-08-20 15:57:46 +0100

    beryl: Use libutils-v32 for libmtkcam_stdutils.so
    * Abort message: 'incStrongRequireStrong() called on 0xb400007c5afa7250 which isn't already owned'
    
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>
    Change-Id: I098c7727e114781775d1b1d8f4e2c9e64bc94e3a
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -98,6 +98,11 @@ blob_fixups: blob_fixups_user_type = {
     ): blob_fixup()
     .patchelf_version(patchelf_version)
     .add_needed("liblog.so"),
+    (
+        "vendor/lib64/mt6855/libmtkcam_stdutils.so"
+    ): blob_fixup()
+    .patchelf_version(patchelf_version)
+    .replace_needed("libutils.so", "libutils-v32.so"),
     "vendor/lib64/hw/mt6855/vendor.mediatek.hardware.pq@2.15-impl.so": blob_fixup()
     .patchelf_version(patchelf_version)
     .add_needed("libshim_sensors.so")
