27c221953e96699f844ae25978e79db850ff5653
ChrisCatto <chriscatto08@gmail.com>
2024-06-19 17:25:19 +0300

    beryl: Link mediatek camera provider against v32 `libutils`
    * Abort message: 'incStrongRequireStrong() called on 0xb40000770d0587a0
      which isn't already owned'
    * Required after U QPR3
    
    Change-Id: Ie6ea7f331207f604cb87804c67f4de9fd9a60d76
    Signed-off-by: ChrisCatto <chriscatto08@gmail.com>
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -99,7 +99,8 @@ blob_fixups: blob_fixups_user_type = {
     .patchelf_version(patchelf_version)
     .add_needed("liblog.so"),
     (
-        "vendor/lib64/mt6855/libmtkcam_stdutils.so"
+        "vendor/lib64/mt6855/libmtkcam_stdutils.so",
+        "vendor/lib64/hw/mt6855/android.hardware.camera.provider@2.6-impl-mediatek.so"
     ): blob_fixup()
     .patchelf_version(patchelf_version)
     .replace_needed("libutils.so", "libutils-v32.so"),
