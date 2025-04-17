5def1a989e3573507849dece289ec483b8943cc6
xyzuniverse <dev.xyzuniverse@gmail.com>
2025-04-18 04:26:02 +0800

    beryl: Initial overlays - Brightness, FOD, Padding got from plato/mt6895-common.
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -258,6 +258,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+# Overlays
+PRODUCT_PACKAGES += \
+    FrameworksResOverlayBeryl \
+    SettingsResOverlayBeryl \
+    SystemUIOverlayBeryl \
+    WifiResOverlayBeryl
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
